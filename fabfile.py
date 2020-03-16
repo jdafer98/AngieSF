ee9a1527c92f3830642e052e05111a9bc70eb6af
jdafer98 <jdafer98@gmail.com>
2020-03-16 23:22:50 +0100

    aniadido fabfile closes #3

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,12 @@
 from fabric import task
 from invoke import run
 
+#Instalación de dependencias
 @task
 def build(name):
 	run("pip install -r requirements.txt")
 
+#evaluación con test
 @task
 def test(name):
 	run("pytest")
