0bfc970e4671a39f91de9911c2aa2d6ef011c4f2
jdafer98 <jdafer98@gmail.com>
2020-04-04 23:18:49 +0200

    probando cosas #5

--- tests/test_angieps.py
+++ tests/test_angieps.py
@@ -1,11 +1,9 @@
 import sys
-import os
 
-#sys.path.append(os.system('pwd')+her)
+#sys.path.append()
 #import ejemplo
 
 
 def test_se_crean_todos_los_campos():
-    os.system("pwd")
     #test_pass = ejemplo.ejemplo()
     assert True
