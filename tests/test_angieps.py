28ece1d919ff4dad8be10198c21f0abc2c219b87
jdafer98 <jdafer98@gmail.com>
2020-04-06 12:18:08 +0200

    Añadiendo test de mole #6

--- tests/test_angieps.py
+++ tests/test_angieps.py
@@ -5,7 +5,7 @@ sys.path.append('/home/travis/build/jdafer98/AngieSF/angieps')
 from angie_colors import AngieColors
 from angie_task import AngieTask
 from port_scanner import PortScanner
-
+from mole import Mole
 
 ## AngieTask
 
@@ -34,3 +34,21 @@ def test_las_id_son_distintas_y_consecutivas():
 
     test_pass = task_list == list(range(1,n_tasks+1))
     assert test_pass
+
+## AngieMole
+
+def test_scan_port():
+    ip = '127.0.0.1'
+    port = 80
+
+    test_pass = False
+    r = Mole.scan_port(ip,port,False)
+
+    if not r[1] and r[0] == port:
+        test_pass = True
+
+    assert test_pass
+
+def test_ping():
+    host = 'www.google.es'
+    assert Mole.ping(host)
